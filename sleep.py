c1ba08a743e43153321ca423e5b66810bbeef160
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-01-09 09:56:19 +0800

    prepared the data, changed data to smaller datatypes and categories

--- sleep.py
+++ sleep.py
@@ -7,6 +7,28 @@ import scipy.stats as stats
 # Load the data
 sleep = pd.read_csv('sleep.csv')
 
-sleep.columns = ['id', 'gender', 'age', 'occupation', 'duration', 'quality_level', 'physical_level', 'stress_level', 'bmi_category', 'bp', 'heart_rate', 'daily_steps', 'sleep_disorder']
-sleep.plot(x = 'sleep_disorder', y = 'duration', kind = 'bar')
-plt.show()
+sleep_transformed = sleep.copy()
+
+# Transform the data
+sleep_transformed['Person ID'] = sleep_transformed['Person ID'].astype('int16')
+sleep_transformed['Gender'] = sleep_transformed['Gender'].astype('category')
+sleep_transformed['Age'] = sleep_transformed['Age'].astype('int16')
+sleep_transformed['Occupation'] = sleep_transformed['Occupation'].astype('category')
+sleep_transformed['Sleep Duration (hours)'] = sleep_transformed['Sleep Duration (hours)'].astype('float32')
+sleep_transformed['Physical Activity Level (minutes/day)'] = sleep_transformed['Physical Activity Level (minutes/day)'].astype('int16')
+sleep_transformed['Stress Level (scale: 1-10)'] = sleep_transformed['Stress Level (scale: 1-10)'].astype('category')
+sleep_transformed['BMI Category'] = sleep_transformed['BMI Category'].astype('category')
+sleep_transformed['Heart Rate (bpm)'] = sleep_transformed['Heart Rate (bpm)'].astype('int16')
+sleep_transformed['Daily Steps'] = sleep_transformed['Daily Steps'].astype('int16')
+sleep_transformed['Sleep Disorder'] = sleep_transformed['Sleep Disorder'].astype('category')
+
+#Round up quality of sleep > int > category
+sleep_transformed['Quality of Sleep (scale: 1-10)'] = sleep_transformed['Quality of Sleep (scale: 1-10)'].apply(lambda x: np.ceil(x)).astype('int16').astype('category')
+
+# Reorder the categories
+sleep_transformed['Quality of Sleep (scale: 1-10)'] = sleep_transformed['Quality of Sleep (scale: 1-10)'].cat.set_categories(new_categories = [1,2,3,4,5,6,7,8,9,10], ordered = True)
+sleep_transformed['Stress Level (scale: 1-10)'] = sleep_transformed['Stress Level (scale: 1-10)'].cat.set_categories(new_categories = [1,2,3,4,5,6,7,8,9,10], ordered = True)
+sleep_transformed['BMI Category'] = sleep_transformed['BMI Category'].cat.set_categories(new_categories = ['Underweight', 'Normal', 'Overweight', 'Obese'], ordered = True)
+
+print(sleep.info())
+print(sleep_transformed.info())
