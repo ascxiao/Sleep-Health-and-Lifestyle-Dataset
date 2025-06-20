53beda5125a7fc2f602cb3c7fb49b4384a3cc3dd
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-06-20 22:34:06 +0800

    Addes pages folder, started to modularize sections

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 import pandas as pd
 import streamlit as st
 import datetime
-from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
 
 df = pd.read_csv('sleep.csv')
 st.set_page_config(layout='wide')
 st.markdown('<style>div.block-container</style>', unsafe_allow_html=True)
-image = Image.open('Sleep_picture.jpg')
+
+if "page" not in st.session_state:
+    st.session_state.page = "Overview"
 
 sidebar = st.sidebar
 
 with sidebar:
-
     st.markdown('<center><h2>Sleep Health Data Analysis</h2></center>', unsafe_allow_html=True)
 
-    if sidebar.button('Overview', use_container_width=True):
-        st.session_state.page == "Overview"
+    if st.button('Overview', use_container_width=True):
+        st.session_state.page = "Overview"
 
-    if sidebar.button('Dashboard', use_container_width=True):
-        st.session_state.page == "Dashboard"
+    if st.button('Dashboard', use_container_width=True):
+        st.session_state.page = "Dashboard"
+
 
 if st.session_state.page == "Overview":
-    col1, col2 = st.columns([0.5, 0.5]);
-    
-    with col1:
-        st.image(image, width=500)
-    with col2:
-        st.markdown
+    from pages import overview_page
+    overview_page.render()
 
 
-if st.session_state.page == "Dashboard":
+elif st.session_state.page == "Dashboard":
     st.title("📖 Dashboard")
     st.write("Intro to gsdgdfg course...")
